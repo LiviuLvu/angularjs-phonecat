3111c112da2b34e3a7eff381f76e6f427fe7777d
liviulvu <liviuiancu@gmx.com>
2017-12-03 23:44:37 +0200

    refactoring step

--- app/phone-list/phone-list.component.spec.js
+++ app/phone-list/phone-list.component.spec.js
@@ -1,5 +1,5 @@
 describe('phoneList', () => {
-    beforeEach(module('phonecatApp'));
+    beforeEach(module('phoneList'));
     describe('PhoneListController', () => {
         it('should create a `phones` model with 3 phones', inject( ($componentController) => {
             let ctrl = $componentController('phoneList');
