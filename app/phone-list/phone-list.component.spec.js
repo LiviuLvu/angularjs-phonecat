55d21718fcd008addaec54ebd3706acd48275109
liviulvu <liviuiancu@gmx.com>
2017-12-04 15:50:59 +0200

    added sorting options

--- app/phone-list/phone-list.component.spec.js
+++ app/phone-list/phone-list.component.spec.js
@@ -1,9 +1,15 @@
 describe('phoneList', () => {
     beforeEach(module('phoneList'));
     describe('PhoneListController', () => {
-        it('should create a `phones` model with 3 phones', inject( ($componentController) => {
-            let ctrl = $componentController('phoneList');
-            expect(ctrl.phones.length).toBe(3);
+        var ctrl;
+        beforeEach(inject(($componentController) => {
+            ctrl = $componentController('phoneList');
         }));
+        it('should create a `phones` model with 3 phones', () => {
+            expect(ctrl.phones.length).toBe(3);
+        });
+        it('should should set a default value for the `orderProp` model', () => {
+            expect(ctrl.orderProp).toBe('age');
+        });
     });
 });
--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -1,33 +1,40 @@
-'use strict';
-
-// Angular E2E Testing Guide:
-// https://docs.angularjs.org/guide/e2e-testing
-
-describe('My app', function() {
-
-  beforeEach(function() {
-    browser.get('index.html');
-  });
-
-});
-
 describe('PhoneCat Application', () => {
-  describe('phoneList', () => {
-    beforeEach(() => {
-      browser.get('index.html');
+    describe('phoneList', () => {
+        beforeEach(() => {
+            browser.get('index.html');
+        });
+        it('should filter the phone list as a user types into the search box', () => {
+            var phoneList = element.all(by.repeater('phone in $ctrl.phones'));
+            var query = element(by.model('$ctrl.query'));
+
+            expect(phoneList.count()).toBe(3);
+
+            query.sendKeys('nexus');
+            expect(phoneList.count()).toBe(1);
+
+            query.clear();
+            query.sendKeys('motorola');
+            expect(phoneList.count()).toBe(2);
+        });
+        it('should be possible to control phone order via the drop-down menu', () => {
+            var queryField = element(by.model('$ctrl.query'));
+            var orderSelect = element(by.model('$ctrl.orderProp'));
+            var nameOption = orderSelect.element(by.css('option[value="name"]'));
+            var phoneNameColumn = element.all(by.repeater('phone in $ctrl.phones').column('phone.name'));
+            
+            function getNames() {
+                return phoneNameColumn.map((elem) => elem.getText());
+            }
+            queryField.sendKeys('tablet');
+            expect(getNames()).toEqual([
+                'Motorola XOOM\u2122 with Wi-Fi',
+                'MOTOROLA XOOM\u2122'
+            ]);
+            nameOption.click();
+            expect(getNames()).toEqual([
+                'MOTOROLA XOOM\u2122',
+                'Motorola XOOM\u2122 with Wi-Fi'
+            ]);
+        });
     });
-    it('should filter the phone list as a user types into the search box', () => {
-      var phoneList = element.all(by.repeater('phone in $ctrl.phones'));
-      var query = element(by.model('$ctrl.query'));
-
-      expect(phoneList.count()).toBe(3);
-
-      query.sendKeys('nexus');
-      expect(phoneList.count()).toBe(1);
-
-      query.clear();
-      query.sendKeys('motorola');
-      expect(phoneList.count()).toBe(2);
-    });
-  });
 });
