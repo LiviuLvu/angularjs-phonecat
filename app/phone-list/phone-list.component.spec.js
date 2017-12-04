9b0f8199aa3c1cae3cbf0396bd2dcedd6396188e
liviulvu <liviuiancu@gmx.com>
2017-12-04 17:40:30 +0200

    added $http service with DI to populate list

--- app/phone-list/phone-list.component.spec.js
+++ app/phone-list/phone-list.component.spec.js
@@ -1,14 +1,22 @@
+'use strict';
 describe('phoneList', () => {
     beforeEach(module('phoneList'));
     describe('PhoneListController', () => {
         var ctrl;
-        beforeEach(inject(($componentController) => {
+        beforeEach(inject(($componentController, _$httpBackend_) => {
+            $httpBackend = _$httpBackend_;
+            $httpBackend.expectGET('phones/phones.json')
+                        .respond([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
+      
             ctrl = $componentController('phoneList');
         }));
-        it('should create a `phones` model with 3 phones', () => {
-            expect(ctrl.phones.length).toBe(3);
+        it('should create a `phones` property with 2 phones fetched with `$http`', function() {
+            expect(ctrl.phones).toBeUndefined();
+          
+            $httpBackend.flush();
+            expect(ctrl.phones).toEqual([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
         });
-        it('should should set a default value for the `orderProp` model', () => {
+        it('should set a default value for the `orderProp` property', function() {
             expect(ctrl.orderProp).toBe('age');
         });
     });
