f04d40602b00e0b7e5fd3f06b3fc7f8e39932a6b
liviulvu <liviuiancu@gmx.com>
2017-12-05 17:47:43 +0200

    added animations to list and detail page

--- app/app.module.js
+++ app/app.module.js
@@ -1,2 +1,2 @@
 'use strict';
-angular.module('phonecatApp', ['ngRoute', 'phoneDetail', 'phoneList', 'core']);
+angular.module('phonecatApp', ['ngRoute', 'phoneDetail', 'phoneList', 'core', 'ngAnimate']);
